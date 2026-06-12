fae97beee5a7fed368f184f977e24407621170b5
matt <audinm1@unlv.nevada.edu>
2026-06-12 13:05:42 -0700

    updated to matthew

--- user/printname.c
+++ user/printname.c
@@ -7,7 +7,7 @@ int main(void)
 {
     //Printf is used to print strings. 
     //Replace the below statement with your name
-    printf("Hello, my name is Sam\n");
+    printf("Hello, my name is Matthew\n");
 
     exit(0); //Exit the program with exit code 0
 }
